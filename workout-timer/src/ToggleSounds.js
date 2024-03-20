af3c151abe499d3c8ba505549df9de52f238b1e2
yamashita.rintaro@R0141git <yamashita.rintaro@i3design.co.jp>
2024-03-20 12:53:56 +0900

    feat: メモ化で不要な再レンダリングを防ぐ

---
+++ workout-timer/src/ToggleSounds.js
@@ -0,0 +1,15 @@
+import { memo } from "react";
+
+function ToggleSounds({ allowSound, setAllowSound }) {
+  return (
+    <button
+      className="btn-sound"
+      onClick={() => setAllowSound((allow) => !allow)}
+    >
+      {allowSound ? "🔈" : "🔇"}
+    </button>
+  );
+}
+
+// propsが変化しない限り、再レンダリングさせない
+export default memo(ToggleSounds);
