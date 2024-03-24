27afb9a2090bd738180b408d87280d95472f2ec3
yamashita.rintaro@R0141git <yamashita.rintaro@i3design.co.jp>
2024-03-24 16:02:58 +0900

    feat: rtk導入

--- redux-intro/src/index.js
+++ redux-intro/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import './store.js';
+import { Provider } from "react-redux";
+import store from './store.js';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
